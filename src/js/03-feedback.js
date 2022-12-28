e55434e4a315a54c1c30ca7f562be9a9977eebbb
DeadlyPants11 <ostrjanin.rostik@gmail.com>
2022-12-28 18:04:24 +0200

    done work

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = form.querySelector('input');
+const message = form.querySelector('textarea');
+
+form.addEventListener('input', throttle(OnInput, 500));
+form.addEventListener('submit', OnSubmit);
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+populateForm();
+
+function OnInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function OnSubmit(evt) {
+    evt.preventeDefault();
+
+    const savedFormParams = localStorage.getItem(LOCALSTORAGE_KEY);
+    const parsFormParams = JSON.parse(savedFormParams);
+    console.log(parsFormParams);
+
+    evt.target.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function populateForm() {
+    const savedFormParams = localStorage.getItem(LOCALSTORAGE_KEY);
+    const parsFormParams = JSON.parse(savedFormParams);
+
+    if(parsFormParams) {
+        input.value = parsFormParams.email || "";
+        message.value = parsFormParams.message || "";
+    }
+}
